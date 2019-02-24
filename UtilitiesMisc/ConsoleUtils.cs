44f673aa93b7e896b70a683cb026099440c8d1dd
rebeckanylander <rebecka.nylander@hotmail.com>
2019-02-24 10:22:24 +0100

    Issue #13: Add the beginning of a class library for handling e.g. prints to Console

---
+++ UtilitiesMisc/ConsoleUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace UtilitiesMisc
+{
+    public class ConsoleUtils
+    {
+        public enum MessageOriginColor
+        {
+            Text = ConsoleColor.White,
+            System = ConsoleColor.DarkGray,
+            Error = ConsoleColor.Red,
+            Default = ConsoleColor.White
+        }
+
+        public static void Print(string text, MessageOriginColor messageOrigin, bool newLineAfterPrint)
+        {
+            ConsoleColor currentColor = Console.ForegroundColor;
+            ConsoleColor color = (ConsoleColor)messageOrigin;
+            Console.ForegroundColor = color;
+            if (newLineAfterPrint)
+            {
+                Console.WriteLine(text);
+            }
+
+            else
+            {
+                Console.Write(text);
+            }
+            Console.ForegroundColor = currentColor;
+        }
+    }
+}
