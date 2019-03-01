81ab22a0e473915e56330361c330339ecd50e514
Alex k <alekra76@kth.se>
2019-03-01 19:48:28 +0100

    #18 Struktur

--- Setup Manager/ViewModels/Initialize.cs
+++ Setup Manager/ViewModels/Initialize.cs
@@ -1,13 +1,8 @@
-﻿using Setup_Manager.ViewModels;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace Setup_Manager
+namespace Setup_Manager.ViewModels
 {
 	class Initialize : ObservableObject
 	{
---
+++ Setup Manager/Views/SetupControl.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Setup_Manager.Views
+{
+	public partial class SetupControl
+	{
+		public SetupControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
