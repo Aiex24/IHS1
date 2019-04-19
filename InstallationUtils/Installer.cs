52bffab31495dc03fc815f69629f7cd2a05f9bde
rebeckanylander <rebecka.nylander@hotmail.com>
2019-04-19 13:59:17 +0200

    Issue #35: Add a class library for the installation managers to use.

---
+++ InstallationUtils/Installer.cs
@@ -0,0 +1,34 @@
+﻿using DbHandler;
+using System;
+using UtilitiesMisc;
+
+namespace InstallationUtils
+{
+    public class Installer
+    {
+        public static Tuple<bool, string> CheckVersion(string dbName, string connString, string currentVersion)
+        {
+            var query = "GetCurrentVersion";
+            var versionReader = DbHandler.DbHandler.ExecuteReader(connString, query, true);
+            if (versionReader != null)
+            {
+                while (versionReader.Read())
+                {
+                    var versionUsedOnServer = (string)versionReader["VersionNumber"];
+                    ConsoleUtils.Print($"The currently installed version is {currentVersion}", MessageOriginColor.System, true);
+                    var usingLatestVersion = int.Parse(versionUsedOnServer.Replace(".", "")) == int.Parse(currentVersion.Replace(".", ""));
+                    return new Tuple<bool, string>(usingLatestVersion, currentVersion);     
+                }
+            }
+
+            return null;
+        }
+
+        public static bool Install(string dbName, string connString)
+        {
+            var result = DbHandler.DbHandler.ExecuteNonQuery(connString, "CREATE DATABASE {dbName}", false);
+            var installationSucceeded = result > -1;
+            return installationSucceeded;
+        }
+    }
+}
